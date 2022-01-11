2966ed88a51f5a013b09e6b62d2e2a9f98d27af3
Simi Prambos <simi.prambos@gmail.com>
2022-01-12 04:35:47 +0700

    feat: add user repository

---
+++ lib/data/repositories/user_repository.dart
@@ -0,0 +1,35 @@
+import 'package:suitmedia/data/models/models.dart';
+import 'package:suitmedia/data/services/services.dart';
+
+abstract class UserRepository {
+  Future<Pagination<User>> findAll({int page = 1});
+
+  Future<User?> findById();
+}
+
+class UserRepositoryImpl implements UserRepository {
+  UserRepositoryImpl(this.api);
+
+  final ApiService api;
+
+  @override
+  Future<Pagination<User>> findAll({int page = 1}) async {
+    final response = await api.get<Map<String, dynamic>>(
+      '/users',
+      queryParameters: <String, dynamic>{'page': page, 'per_page': 10},
+    );
+    if (response.statusCode != 200 || response.data == null) {
+      throw Exception(response.statusMessage);
+    }
+    return Pagination.fromMap(
+      response.data!,
+      (value) => User.fromMap(value),
+    );
+  }
+
+  @override
+  Future<User?> findById() {
+    // TODO: implement findById
+    throw UnimplementedError();
+  }
+}
